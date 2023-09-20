95fecc67236403cdfcd01805aa12118186d08179
PallabRoy-Cy <pallabr8@gmail.com>
2023-09-20 19:05:51 +0600

    Initial commit

---
+++ Assets/SpawnProjectile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour
+{
+    public GameObject firePoint;
+    public List<GameObject> vfx = new List<GameObject> ();
+    public RotationByMouse rotateToMouse;
+
+    private GameObject effectToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        effectToSpawn = vfx [0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton (0)){
+            Spawnvfx();
+
+        }
+    }
+    void Spawnvfx(){
+        GameObject vfx;
+        if(firePoint != null){
+            vfx = Instantiate(effectToSpawn, firePoint.transform.position, Quaternion.identity);
+
+        if (rotateToMouse != null){
+        vfx.transform.localRotation = rotateToMouse.GetRotation ();
+        }
+        }else
+        {
+            Debug.Log("No File Point");
+        }
+    }
+}
